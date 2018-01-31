3216d6d9b9e39d52dabf58b0d4a5f053d41a8da5
rosemer <35221895+rosemer@users.noreply.github.com>
2018-01-31 15:47:15 +0530

    primeb.C
    PROGRAM FOR PRIME NUMBER BETWEEN TWO INTERVELS

---
+++ primeb.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void main()
+{
+    int l, h, i, f;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &l, &h);
+
+    printf("Prime numbers between %d and %d are: ", l, h);
+
+    while (l< h)
+    {
+        flag = 0;
+
+        for(i = 2; i <= low/2; ++i)
+        {
+            if(l % i == 0)
+            {
+                f= 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", l);
+
+        ++l;
+    }
+
+    getch();
+}
