fd1c8af150c2fe3d95a41b81add0913f4dcdeeba
rosemer <35221895+rosemer@users.noreply.github.com>
2018-02-05 16:41:52 +0530

    medianelement.C
    program for an array of an median element

---
+++ medianelement.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("\nEnter the number of elements");
+scanf("%d",&b);
+printf("\nEnter The elements are:");
+scanf("%d",&a);
+for(c=0;c<a;c++)
+{
+printf("%d",a);
+}
+getch();
+}
