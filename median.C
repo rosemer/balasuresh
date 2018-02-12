9c167ca3fe7a7597277827419f9dbdc7b43bd333
rosemer <35221895+rosemer@users.noreply.github.com>
2018-02-12 16:15:37 +0530

    median.C
    MEDIAN OF AN ARRAY

---
+++ median.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,rev=0;
+printf("\nenter the number:");
+scanf("%d",&a);
+while(n!=0)
+{
+re=rev*10;
+re=re+n%10;
+a=a/10;
+}
+printf("\nthe sorted array is:",rev);
+return(0);
+}
