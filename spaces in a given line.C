38a4b6e200ee0b4c702b44b24a33d26f8da8810e
rosemer <35221895+rosemer@users.noreply.github.com>
2018-02-21 16:16:38 +0530

    spaces in a given line.C
    program  for  space ina given line

---
+++ spaces in a given line.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,n=0;
+char s[100];
+printf("\nEnter the sentense");
+gets(s);
+for(a=0;s[a]!=0;a++)
+{
+if(s[a]==' ')
+n++;
+}
+printf("\nThe number of space in the sentences are %d",n);
+}
