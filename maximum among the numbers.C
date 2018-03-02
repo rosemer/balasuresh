2b96592e0ca75290acd81d72da0a2074b4b10a62
rosemer <35221895+rosemer@users.noreply.github.com>
2018-03-02 02:31:00 -0800

    maximum among the numbers.C
    program for maximum among the numbers

---
+++ maximum among the numbers.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i.j,b,c;
+printf("\n enter the number of elements:");
+scanf("%d",&a);
+printf("\n enter the elements:");
+for(i=0;i<a;i++);
+{
+scanf("%d",&i);
+{
+if(a[j]>a[j+1])
+{
+c=a[j];
+a[j]=a[j+1];
+a[j+1]=c;
+}
+printf("\n maximum among the number is%d"'a[n-1]);
+}
+getch();
+}
+}
+
