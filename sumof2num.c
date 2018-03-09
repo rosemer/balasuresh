2cbee4b3afafff406a7655d9ea1eff254b129483
rosemer <35221895+rosemer@users.noreply.github.com>
2018-03-09 16:53:52 +0530

    sumof2num.c
    sum program

---
+++ sumof2num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("\n enter the a value");
+scanf("%d",&a);
+printf("\n enter the b value");
+scanf("%d",&b);
+{
+c=a+b;
+{
+printf("\n addition of two numbers%d,c);
+}
+getch();
+}
+}
+
