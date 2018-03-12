f4a79466e9bf79681432aacaa6fa1811dc1c07f8
rosemer <35221895+rosemer@users.noreply.github.com>
2018-03-12 15:59:45 +0530

    swap.cpp
    program for swapping of two numbers

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+  #include<iostream.h>
+  #include<conio.h>
+  void main()
+  {
+  int a,b,temp;
+  clrscr();
+  cout<<"enter the two numbers";
+  cin>>a>>b;
+  {
+  temp=a;
+  a=b;
+  b=temp;
+  }
+  cout<<"swaping of two numbers,a,b";
+  getch();
+  }
+
