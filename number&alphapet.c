1ddb9d9ee3a8de1284116fd7e55bdc87767994dd
rosemer <35221895+rosemer@users.noreply.github.com>
2018-03-28 16:20:41 +0530

    number&alphapet.c
    alphapet

---
+++ number&alphapet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+ char str[50]="helloworld123";
+ int i;
+ for(i=0;str[i]='\0';i++)
+ {
+  if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')||(str[i]>=0&&str[i]<=9))
+  {
+    printf("yes");
+    }
+    }
+    getch();;
+    }
